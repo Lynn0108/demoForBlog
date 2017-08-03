41c9ae25fd41085d6cc90c0474e33dbedde03264
liaorui <liaorui@yidian-inc.com>
2017-08-03 18:54:40 +0800

    不平滑滚动

--- demo0802/main.js
+++ demo0802/main.js
@@ -1,61 +1,80 @@
 var main = {
+	ctx: document.querySelector('.content'),
+	nav: document.querySelector('.nav'),
+	imgDivArr: [],
+	len: 0,
+	imgDivH: [],
+	imgDivOt: [],
 
-	setMarginBottom: function () {
-
-	}
-}
-
-
-var imgDivArr = document.querySelectorAll('.imgDiv');
-var len = imgDivArr.length;
-var imgDivH = [];
-var imgDivOt = [];
-for (var i = 0; i < len; i++) {
-	imgDivH[i] = imgDivArr[i].offsetHeight;
-	imgDivOt[i] = imgDivArr[i].offsetTop;
-}
-var ctxH = document.querySelector('.content').offsetHeight;
-var marginB = ctxH - imgDivH[len - 1] + 40 ;
-imgDivArr[len - 1].style.marginBottom = marginB + 'px';
-
-var nav = document.querySelector('.nav');
-nav.addEventListener('click', function(event) {
-	var e = event || window.event;
-	var target = e.target || e.srcElement;
-	// console.log(e);
-	if (target.nodeName.toLowerCase() === 'li') {
-		this.querySelector('.active').className = '';
-		target.className = 'active';
-	}
-	var index = getIndex(target);
-	contentRoll(index);
-})
-
-function getIndex(ele) {
-	//IE is simplest and fastest 
-	if(ele.sourceIndex){ 
-		return ele.sourceIndex - ele.parentNode.sourceIndex - 1; 
-	} 
-	//other browsers 
-	var i = 0; 
-	while(ele = ele.previousElementSibling){ 
-		i++; 
-	} 
-	return i; 
-}
-
-function contentRoll(index) {
-
-}
-
-var ctx = document.querySelector('.content');
-ctx.addEventListener('scroll', function() {
-	console.log(this.scrollTop);
-	for (var i = 0; i < len; i++) {
-		if (this.scrollTop < imgDivH[i] + imgDivOt[i] - 40) {
-			nav.querySelector('.active').className = '';
-			nav.querySelectorAll('li')[i].className = 'active';
-			break;
+	init: function() {
+		var _this = this;
+		this.getDivH();
+		this.setMarginBottom();
+		this.ctx.addEventListener('scroll', function() {
+			// console.log(this.scrollTop);
+			_this.activeLi();
+			
+		})
+		this.nav.addEventListener('click', function(event) {
+			var e = event || window.event;
+			var target = e.target || e.srcElement;
+			if (target.nodeName.toLowerCase() === 'li') {
+				/*this.querySelector('.active').className = '';
+				target.className = 'active';*/
+				_this.clickToRoll(target);
+			}
+			
+		})
+	}, 
+	clickToRoll: function(target) {
+		var index = _this.getIndex(target);
+		_this.contentRoll(index);
+	},
+	activeLi: function() {
+		for (var i = 0; i < _this.len; i++) {
+			if (this.ctx.scrollTop <= _this.imgDivH[i] + _this.imgDivOt[i] - 40) {
+				console.log(i);
+				_this.nav.querySelector('.active').className = '';
+				_this.nav.querySelectorAll('li')[i].className = 'active';
+				break;
+			}
 		}
+	},
+	getDivH: function() {
+		this.imgDivArr = document.querySelectorAll('.imgDiv');
+		this.len = this.imgDivArr.length;
+		this.imgDivH = [];
+		this.imgDivOt = [];
+		for (var i = 0; i < this.len; i++) {
+			this.imgDivH[i] = this.imgDivArr[i].offsetHeight;
+			this.imgDivOt[i] = this.imgDivArr[i].offsetTop;
+		}
+	},
+	setMarginBottom: function () {
+		var ctxH = this.ctx.offsetHeight;
+		var marginB = ctxH - this.imgDivH[this.len - 1] + 40 ;
+		this.imgDivArr[this.len - 1].style.marginBottom = marginB + 'px';
+	},
+	getIndex: function (ele) {
+		//IE is simplest and fastest 
+		if(ele.sourceIndex){ 
+			return ele.sourceIndex - ele.parentNode.sourceIndex - 1; 
+		} 
+		//other browsers 
+		var i = 0; 
+		while(ele = ele.previousElementSibling){ 
+			i++; 
+		} 
+		return i; 
+	},
+	contentRoll: function (i) {
+		if (i > 0) {
+			this.ctx.scrollTop = this.imgDivH[i-1] + this.imgDivOt[i-1];
+		} else {
+			this.ctx.scrollTop = 0;
+		}
+		
 	}
-})
+
+}
+main.init();
